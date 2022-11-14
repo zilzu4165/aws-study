2c13cf57ab06a45b772f18067410cc04e10ece5e
zilzu <zilzu4165@naver.com>
2022-11-14 22:52:24 +0900

    chapter 3 Auditing 기능

--- src/main/java/me/zilzu/springboot/domain/posts/Posts.java
+++ src/main/java/me/zilzu/springboot/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package me.zilzu.springboot.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import me.zilzu.springboot.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/me/zilzu/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/me/zilzu/springboot/domain/posts/PostsRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,5 +40,24 @@ class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    void BaseTimeEntity_등록() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title").content("content").author("author").build());
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>> createDate=" + posts.getCreatedTime() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedTime()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+
     }
 }
